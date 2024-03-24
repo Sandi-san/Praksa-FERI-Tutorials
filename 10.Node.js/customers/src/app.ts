e41f3087b1a31bd7a78cd297186d3d0e85ace38d
Sandi-san <nicksrebotsandi@gmail.com>
2024-03-24 10:11:57 +0100

    Tutorial #10 Node.js Finale - TypeScript in Node.js

--- 10.Node.js/customers/src/app.ts
+++ 10.Node.js/customers/src/app.ts
@@ -1,20 +1,10 @@
-"use strict";
 //uporabi paket uuid (npm install uuid)
 // const uuid = require('uuid');
 //dostop do objekta v paketu
 // const { v4: uuidv4 } = require('uuid');
 // console.log(uuidv4());
 // import { v4 as uuidv4 } from 'uuid'; //react sintaksa (dodaj "type": "module" v package.json)
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+
 //uporabi paket express
 const express = require('express');
 //uporabi paket mongoose (connection z mongodb)
@@ -23,54 +13,64 @@ const mongoose = require('mongoose');
 //povezi customer models folder
 // const Customer = require('./models/customer');
 // import z es6
-const customer_1 = require("./models/customer");
+import {Customer} from './models/customer';
 //uporabi paket cors
 const cors = require('cors');
+import {Request, Response} from 'express';
+
 const app = express(); //shrani express paket v spremenljivko
 mongoose.set('strictQuery', false);
+
 app.use(cors());
 //Za parsat json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 //uporabi paket dotenv
 //Node.js ni v production stanju, uporabi .env spremenljivke
 if (process.env.NODE_ENV !== 'production') {
     //uporaba .env spremenljivk (environment vars) v projektu
     require('dotenv').config();
 }
+
 const PORT = process.env.PORT || 3000;
 //povezava baze
 const CONNECTION = process.env.CONNECTION;
-const newCustomer = new customer_1.Customer({
+
+const newCustomer = new Customer({
     name: 'Marjan',
     industry: 'Default customer'
 });
+
 //endpoint (povratna informacija ko user obisce port)
 //browser po defaultu uporabi get
 //INDEX.HTML
-app.get('/', (req, res) => {
+app.get('/', (req : Request, res : Response) => {
     res.send("Welcome to my site!!");
 });
-app.post('/', (req, res) => {
+
+app.post('/', (req : Request, res : Response) => {
     // res.send('This is a post request!');
     res.send(newCustomer);
 });
+
 //DOBI VSE CUSTOMERJE
 //dobi customers collection iz DB (UPORABI async)
-app.get('/api/customers', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/api/customers', async (req : Request, res : Response) => {
     //vrne vse collections v console (tabele)
     // console.log(await mongoose.connection.db.listCollections().toArray());
     try {
         // res.send({"data with space":json["favorite People"]})
-        const result = yield customer_1.Customer.find(); //najdi shremo
+        const result = await Customer.find(); //najdi shremo
         res.json({ "customers": result }); //res.send ali res.json
-    }
-    catch (e) {
+
+    } catch (e) {
         res.status(500).json({ error: e.message }); //vrni code 500 in json error
     }
-}));
+});
+
 //DOBI EN CUSTOMER (ID)
-app.get('/api/customers/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/api/customers/:id', async (req : Request, res : Response) => {
     //requestParams = customers/id_val
     //requestQuery = customers?id=id_val (deluje tudi pri: customers/id_val?nov_param=...)
     console.log({
@@ -80,130 +80,131 @@ app.get('/api/customers/:id', (req, res) => __awaiter(void 0, void 0, void 0, fu
     try {
         const customerId = req.params.id;
         console.log(customerId);
-        const customer = yield customer_1.Customer.findById(customerId);
+        const customer = await Customer.findById(customerId);
         console.log(customer);
         if (!customer) {
-            res.status(404).json({ error: 'Item not found' });
+            res.status(404).json({ error: 'Item not found' })
         }
         else {
             res.json({ customer });
         }
+    } catch (e) {
+        res.status(500).json({ error: 'Error: Bad syntax' })
     }
-    catch (e) {
-        res.status(500).json({ error: 'Error: Bad syntax' });
-    }
-}));
+});
+
 //SAVE CUSTOMER
-app.post('/api/customers', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post('/api/customers', async (req : Request, res : Response) => {
     //POZOR: req.body MORE BITI V OBLIKI JSON
     //sicer poslje "name {name,industry}"" sintakso
     console.log(req.body);
-    const customer = new customer_1.Customer(req.body);
+    const customer = new Customer(req.body);
     try {
         console.log(customer);
-        yield customer.save();
+        await customer.save();
         // req.status(201).json({customer}); //vrne atribute z objektom
         res.status(201).json(customer); //vrne le atribute brez imena objekta
-    }
-    catch (e) {
+    } catch (e) {
         res.status(400).json({ error: e.message });
     }
-}));
+});
+
 //UPDATE CUSTOMER (PUT - vse elemente)
-app.put('/api/customers/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.put('/api/customers/:id', async (req : Request, res : Response) => {
     try {
         const customerId = req.params.id;
         //posodobi enega glede _id in poslji novi data
         // const result = await Customer.findOne({ _id: customerId }, req.body);
         //posodobi enega in vrni (POZOR po defaultu vrne OLD data, za NEW data dodaj new: true)
-        const customer = yield customer_1.Customer.findOneAndReplace({ _id: customerId }, req.body, { new: true });
+        const customer = await Customer.findOneAndReplace({ _id: customerId }, req.body, { new: true });
         console.log(customer);
         //vrne count kolikokrat se je objekt posodobil
         // res.json({ updatedCount: result.modifiedCount });
         res.json({ customer });
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e.message);
-        res.status(500).json({ error: 'Something went wrong' });
+        res.status(500).json({ error: 'Something went wrong' })
     }
-}));
+});
+
 //UPDATE CUSTOMER (PATCH - spremeni le dolocne elemente)
-app.patch('/api/customers/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.patch('/api/customers/:id', async (req : Request, res : Response) => {
     try {
         const customerId = req.params.id;
-        const customer = yield customer_1.Customer.findOneAndUpdate({ _id: customerId }, req.body, { new: true });
+        const customer = await Customer.findOneAndUpdate({ _id: customerId }, req.body, { new: true });
         console.log(customer);
         res.json({ customer });
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e.message);
-        res.status(500).json({ error: 'Something went wrong' });
+        res.status(500).json({ error: 'Something went wrong' })
     }
-}));
+});
+
 //DELETE CUSTOMER
-app.delete('/api/customers/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.delete('/api/customers/:id', async (req : Request, res : Response) => {
     try {
         const customerId = req.params.id;
-        const result = yield customer_1.Customer.deleteOne({ _id: customerId });
+        const result = await Customer.deleteOne({ _id: customerId });
         res.json({ deletedCount: result.deletedCount });
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong' })
     }
-    catch (error) {
-        res.status(500).json({ error: 'Something went wrong' });
-    }
-}));
+});
+
 //GET ORDER (nested v CUSTOMER)
-app.get('/api/orders/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get('/api/orders/:id', async (req : Request, res : Response) => {
     try {
         //POZOR: vrne celi customer objekt (tj. najvisji hierarhicni)
-        const result = yield customer_1.Customer.findOne({ 'orders._id': req.params.id });
-        if (result) {
+        const result = await Customer.findOne({'orders._id':req.params.id});
+        if(result){
             res.json(result);
         }
-        else {
+        else{
             res.status(404).json({ error: 'Error: Order not found' });
         }
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e.message);
         res.status(500).json({ error: 'Something went wrong' });
     }
-}));
+});
+
 //UPDATE ORDER (posodobi LE DELE array objektov - NESTED)
-app.patch('/api/orders/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.patch('/api/orders/:id', async (req : Request, res : Response) => {
     try {
         const orderId = req.params.id;
         req.body._id = orderId; //ne spremeni id, ce ni podan v req.body
-        const order = yield customer_1.Customer.findOneAndUpdate(
-        //v customerju, pojdi v orderId od orderja, mongo bo vrnil top document (tj. customer)
-        //POZOR: za nested key-e uporabi single quote ('')
-        { 'orders._id': orderId }, 
-        //referenca na order z orderId-jem
-        { $set: { 'orders.$': req.body } }, { new: true });
+        const order = await Customer.findOneAndUpdate(
+            //v customerju, pojdi v orderId od orderja, mongo bo vrnil top document (tj. customer)
+            //POZOR: za nested key-e uporabi single quote ('')
+            { 'orders._id': orderId },
+            //referenca na order z orderId-jem
+            { $set: { 'orders.$': req.body } },
+            { new: true }
+        );
         console.log(order);
         if (order) {
             res.json({ order });
         }
-        else {
+        else{
             res.status(404).json({ error: 'Error: Order not found' });
         }
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e.message);
         res.status(500).json({ error: 'Something went wrong' });
     }
-}));
+});
+
 //async uporaba mongoose
-const start = () => __awaiter(void 0, void 0, void 0, function* () {
+const start = async () => {
     try {
-        yield mongoose.connect(CONNECTION);
+        await mongoose.connect(CONNECTION);
         //zacetna funkcija ob zagonu serverja (v console)
         app.listen(PORT, () => {
             console.log('App listening on port ' + PORT);
         });
-    }
-    catch (e) {
+    } catch (e) {
         console.log(e.message);
     }
-});
+};
 //invoke function
 start();
--- 10.Node.js/customers/src/models/customer.ts
+++ 10.Node.js/customers/src/models/customer.ts
@@ -1,15 +1,23 @@
-"use strict";
 // const mongoose = require('mongoose');
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Customer = void 0;
+
 //import z es6 (potrebno pri .ts)
-const mongoose_1 = require("mongoose");
-;
-;
+import {HydratedDocument, Schema, model} from 'mongoose';
+
+//definiranje modelov v typescript (interface)
+interface IOrder {
+    description: string,
+    amountInCents?: number //?-optional element
+};
+interface ICustomer {
+    name: string,
+    industry?: string,
+    orders?: IOrder[]
+};
+
 //definiraj model (shemo, struktura)
 // const customerSchema = new mongoose.Schema({
 //uporaba z es6
-const customerSchema = new mongoose_1.Schema({
+const customerSchema = new Schema<ICustomer>({
     name: {
         type: String,
         required: true
@@ -22,10 +30,13 @@ const customerSchema = new mongoose_1.Schema({
         }
     ]
 });
+
 //ustvari collection v mongoDB
 // module.exports = mongoose.model('Customer', customerSchema);
 //uporaba z es6
-exports.Customer = (0, mongoose_1.model)('Customer', customerSchema);
+export const Customer = model('Customer', customerSchema);
+
+
 //uporaba interface-ov. POZOR: treba import HydratedDocument v tem file-u
 // const c: HydratedDocument<ICustomer> = new Customer({
 //     name:'test',
