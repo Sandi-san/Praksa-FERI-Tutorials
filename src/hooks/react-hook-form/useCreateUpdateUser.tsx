e1c05061331fa5756d6897ecd60fd3fd20b4b808
Sandi-san <nicksrebotsandi@gmail.com>
2024-04-09 10:30:54 +0200

    Tutorial #7 - CRUD Users

---
+++ src/hooks/react-hook-form/useCreateUpdateUser.tsx
@@ -0,0 +1,85 @@
+import { yupResolver } from '@hookform/resolvers/yup'
+import { UserType } from 'models/auth'
+import { useForm } from 'react-hook-form'
+import * as Yup from 'yup'
+
+export interface CreateUserFields {
+  first_name?: string
+  last_name?: string
+  email: string
+  password: string
+  confirm_password: string
+  role_id: string
+}
+
+export interface UpdateUserFields {
+  first_name?: string
+  last_name?: string
+  email: string
+  password?: string
+  confirm_password?: string
+  role_id: string
+}
+
+//za update
+interface Props {
+  defaultValues?: UserType
+}
+
+export const useCreateUpdateUserForm = ({ defaultValues }: Props) => {
+  const CreateUserSchema = Yup.object().shape({
+    first_name: Yup.string().notRequired(),
+    last_name: Yup.string().notRequired(),
+    email: Yup.string().email().required('Please enter a valid email'),
+    password: Yup.string()
+      .matches(
+        /^(?=.*\d)[A-Za-z.\s_-]+[\w~@#$%^&*+=`|{}:;!.?"()[\]-]{6,}/,
+        'Password must contain least one number, lower or uppercase letter and must be longer than 6 characters.',
+      )
+      .required(),
+    confirm_password: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords do not match')
+      .required('Passwords do not match'),
+    role_id: Yup.string().required('Role field is required'),
+  })
+
+  const UpdateUserSchema = Yup.object().shape({
+    first_name: Yup.string().notRequired(),
+    last_name: Yup.string().notRequired(),
+    email: Yup.string().email().required('Please enter a valid email'),
+    password: Yup.string().notRequired(),
+    confirm_password: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Passwords do not match')
+      .notRequired(),
+    role_id: Yup.string().notRequired(),
+  })
+
+  const {
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm({
+    defaultValues: {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      confirm_password: '',
+      role_id: '',
+      ...defaultValues,
+    },
+    mode: 'onSubmit',
+    // ce so defaultValues, update, sicer create
+    resolver: defaultValues
+      ? yupResolver(UpdateUserSchema)
+      : yupResolver(CreateUserSchema),
+  })
+
+  return {
+    handleSubmit,
+    errors,
+    control,
+  }
+}
+
+export type CreateUpdateUserForm = ReturnType<typeof useCreateUpdateUserForm>
